c564066d9fd74b36778689b3ce050bcab095b3be
LTVolt <johnssantos7@gmail.com>
2025-07-20 16:58:03 +0100

    First iteration

--- Java-RPG/src/RPG/Characters/EnemyAI.java
+++ Java-RPG/src/RPG/Characters/EnemyAI.java
@@ -4,10 +4,8 @@ public enum EnemyAI {
     
     ATTACK(1), SKILL(2), DEFEND(3);
 
-    private final int enemyAction;
-
     EnemyAI(int enemyAction) {
-        this.enemyAction = enemyAction;
+        
     }
 
 }
